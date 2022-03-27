18f46af129399faab91b6466e13afd6f3e769e47
Beto1821 <beto1821@uol.com.br>
2022-03-27 19:02:00 -0300

    ex05 e 06 rev00

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function randomColors() {
-  const color = ['rgb(0, 0, 0)'];
-  const colors = document.getElementsByClassName('ball');
-  for (let i = 0; i < 5; i += 1) {
+const bola = document.getElementsByClassName('ball');
+const rgbColor = document.getElementById('rgb-color');
+const aColor = Math.floor(Math.random(bola) * 5);
+const paletaCores = document.getElementById('color-palette');
+
+function criaBolas() {
+  for (let i = 0; i < bola.length; i += 1) {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    color.push(`rgb(${r}, ${g}, ${b})`);
-  }
-  for (let z = 0; z < colors.length; z += 1) {
-    colors[z].style.backgroundColor = color[z];
+    bola[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   }
 }
+criaBolas();
 
-randomColors();
+function picAcolor() {
+  const respostaRGB = document.createElement('div');
+  respostaRGB.innerText = getComputedStyle(bola[aColor]).backgroundColor;
+  rgbColor.appendChild(respostaRGB);
+}
+picAcolor();
+
+function seleciona(event) {
+  const selecionado = getComputedStyle(event.target).backgroundColor;
+  return selecionado;
+}
+paletaCores.addEventListener('click', seleciona);
+
+function reset() {
+    location.reload();
+}
+
+const btn = document.getElementById('reset-game');
+btn.addEventListener('click', reset);
