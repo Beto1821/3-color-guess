a4390869c104660b05fde99ba7972130a6d32408
Beto1821 <beto1821@uol.com.br>
2022-03-28 07:39:12 -0300

    ex06 rev10

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-restricted-globals */
 const bola = document.getElementsByClassName('ball');
 const rgbColor = document.getElementById('rgb-color');
 const aColor = Math.floor(Math.random(bola) * 5);
 const paletaCores = document.getElementById('color-palette');
 const resposta = document.getElementById('answer');
 const codigoCor = document.getElementById('rgb-color');
+const respostaRGB = document.createElement('div');
+rgbColor.appendChild(respostaRGB);
+
 
 function criaBolas() {
   for (let i = 0; i < bola.length; i += 1) {
@@ -12,33 +16,24 @@ function criaBolas() {
     const b = Math.floor(Math.random() * 256);
     bola[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   }
+  respostaRGB.innerText = getComputedStyle(bola[aColor]).backgroundColor;
 }
 criaBolas();
 
-function picAcolor() {
-  const respostaRGB = document.createElement('div');
-  respostaRGB.innerText = getComputedStyle(bola[aColor]).backgroundColor;
-  rgbColor.appendChild(respostaRGB);
-}
-picAcolor();
-
-const texto = document.createElement('p');
-resposta.appendChild(texto);
-texto.innerText = 'Escolha uma cor';
-
-function seleciona(event) {
+function textoShow(event) {
   const selecionado = getComputedStyle(event.target).backgroundColor;
+  console.log(selecionado);
   if (selecionado === codigoCor.firstElementChild.innerHTML) {
-    texto.innerText = 'Acertou';
+    resposta.innerText = 'Acertou!';
   } else {
-    texto.innerText = 'Errou! Tente novamente!';
+    resposta.innerText = 'Errou! Tente novamente!';
   }
 }
-paletaCores.addEventListener('click', seleciona);
+paletaCores.addEventListener('click', textoShow);
 
 function reset() {
-  // eslint-disable-next-line no-restricted-globals
-  location.reload();
+  resposta.innerText = 'Escolha uma cor';
+  criaBolas();
 }
 
 const btn = document.getElementById('reset-game');
